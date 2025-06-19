8fc9d327ff002039f8d6be4364485bc2d748cac4
Amrutha Eyunni <amruthalu@gmail.com>
2025-06-19 14:59:22 +0100

    1 valid grid + shuffling columns works

--- sudoku.py
+++ sudoku.py
@@ -7,8 +7,7 @@ def sudokuGenerator(d, row):
       print(" ".join(map(str, x[i:i+d]))) # choosing 3 numbers at a time, applying map function to convert them all to strings at the
                                           # same time and printing them on 1 line using spaces. 
       i += d # incrementing i by d again to choose next set of numbers
-            
-            
+                
 sudokuGenerator(3, list(range(1, 10)))
 
 # initialising a 2D grid with only 0s
@@ -59,3 +58,96 @@ def print_grid(grid):
         print("---------------------------")
         
 print_grid(grid)
+
+print("Printing another grid")
+def print_another_grid(grid):
+    for i in range(len(grid)):
+        if i % 3 == 0:
+            print("---------------------------")
+        j = 0
+        part1 = grid[i][j:j+3]
+        part1_to_s = ' '.join(map(str, part1))
+        part2 = grid[i][j+3:j+6]
+        part2_to_s = ' '.join(map(str, part2))
+        part3= grid[i][j+6:j+9]
+        part3_to_s = ' '.join(map(str, part3))
+        print(" | " + part1_to_s + " | " + part2_to_s + " | " + part3_to_s + " | ")
+    print("---------------------------")
+
+print_another_grid(grid)
+
+# initialise a 1D grid/array:
+MAX_VALUE = 81
+oned_grid = [0] * MAX_VALUE
+
+def shift(given_list, n):
+    i = 0
+    while i < n:
+        given_list.insert(0, given_list.pop())
+        i = i + 1
+    return given_list
+
+print(shift(list(range(1, 10)), 3))
+
+
+indices_to_shift = [0, 3, 6, 1, 4, 7, 2, 5, 8]
+initial_list = list(range(1, 10))
+
+def fill_grid2(oned_grid):
+    for i in range(0, len(oned_grid), 9):
+        initial_list = list(range(1, 10))
+        oned_grid[i:i+9] = shift(initial_list, indices_to_shift[int(i/9)])
+    return oned_grid
+
+print(fill_grid2(oned_grid))
+
+print("Printing the 1D array:")
+print("---------------------------")
+def print_new_grid(grid):
+    for j in range(0, len(grid), 9):
+        if j == 27 or j == 54 or j == 81: # to draw lines only
+            print("---------------------------")
+        part1 = grid[j:j+3]
+        part1_to_s = ' '.join(map(str, part1))
+        part2 = grid[j+3:j+6]
+        part2_to_s = ' '.join(map(str, part2))
+        part3= grid[j+6:j+9]
+        part3_to_s = ' '.join(map(str, part3))
+        print(" | " + part1_to_s + " | " + part2_to_s + " | " + part3_to_s + " | ")
+    print("---------------------------")
+
+print_new_grid(oned_grid)
+
+def extract_column(grid, col_number):
+    new_col = list()
+    for i in range(col_number, len(grid), 9):
+        new_col.append(grid[i])
+    return new_col
+
+#print(extract_column(oned_grid, 8))
+
+def assign_columns(grid, col, col_data):
+    for i in range(len(col_data)):
+        grid[i*9 + col] = col_data[i]
+    return grid
+
+print("This is a grid with shuffled columns:")
+def shuffle_columns(grid, col1, col2, col3):
+    col_indices = [col1, col2, col3]
+    col_dict = {col1: extract_column(grid, col1), 
+                col2: extract_column(grid, col2), 
+                col3: extract_column(grid, col3)}
+    shuffled_indices = list(col_indices)
+    random.shuffle(shuffled_indices)
+    for i in range(len(col_indices)):
+        col_index = col_indices[i]
+        new_index = shuffled_indices[i]
+
+        col_to_move = col_dict[col_index]
+        assign_columns(grid, new_index, col_to_move)
+    return grid
+
+grid1 = shuffle_columns(oned_grid, 0, 1, 2)
+grid3 = shuffle_columns(oned_grid, 3, 4, 5)
+print_new_grid(grid1)
+print_new_grid(grid3)
